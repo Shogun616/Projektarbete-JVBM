c60154563afe2e8e2f16cca7abf18973ef03fdea
maini <philip.mattsson@iths.se>
2020-11-26 19:21:04 +0100

    Added menu test-class

---
+++ src/test/java/jvbm/com/MenuTest.java
@@ -0,0 +1,40 @@
+package jvbm.com;
+
+import org.junit.jupiter.api.*;
+
+public class MenuTest {
+
+    @BeforeAll
+    public static void init(){
+        System.out.println("Before All init() method called");
+    }
+
+    @BeforeEach
+    public void initEach(){
+
+        System.out.println("Before Each initEach() method called");
+    }
+
+    @DisplayName("options test")
+    @Test
+    void testOptions(){
+
+    }
+
+    @DisplayName("menu test")
+    @Test
+    void testMenu(){
+
+    }
+
+    @AfterEach
+    public void cleanUpEach(){
+        System.out.println("After Each cleanUpEach() method called");
+    }
+
+
+    @AfterAll
+    public static void cleanUp(){
+        System.out.println("After All cleanUp() method called");
+    }
+}
